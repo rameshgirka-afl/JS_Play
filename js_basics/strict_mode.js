09747bd4329dd184b1ba43d3a79e2cb3153c3085
rameshgirka-afl <rameshgirka@gmail.com>
2023-02-16 18:56:14 +0530

    commiting 1

---
+++ js_basics/strict_mode.js
@@ -0,0 +1,15 @@
+// Modern JS: previously accepted are - now Errors. || Forbids certain things - bug free 
+// should be first statement - else no re"Strictions" :) 
+'use strict'
+
+
+
+// Global association of undefined variable is NOT allowed any more
+test = "string"
+//fuction(param,param) not allowed 
+
+//mis spelling variables - no alert / linting 
+
+// Var Hoisting - function scoped and not block scoped.. 
+
+//keywords used for future javascript functions 
